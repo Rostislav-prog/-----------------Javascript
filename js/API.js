ba77c1d3989aaf1e87c295c1769957239a08baa9
Rostislav <khmelinin.r@gmail.com>
2021-07-09 11:02:20 +0900

    Lesson_6

---
+++ js/API.js
@@ -0,0 +1,21 @@
+"use strict";
+
+export default {
+    fetch() { // метод призван запрашивать данные с сервера
+        return [
+            { title: 'Куртка', prise: 1500, description: 'Стрёмная синяя куртка.', image: 'product_1.webp', discount: 0 },
+            { title: 'Костюм', prise: 15000, description: 'Чёрный офисный костюм.', image: 'product_2.webp', discount: 0 },
+            { title: 'Кепка', prise: 150, description: 'Кепка для подростка дибила', image: 'product_3.webp', discount: 0 },
+            { title: 'Борода', prise: 150, description: 'Шикарная чёрная борода', image: 'product_4.webp', discount: 0 },
+            { title: 'Товар', prise: 150, description: 'Known for her sculptural takes on traditional tailoring.', image: 'product_5.webp', discount: 0 },
+            { title: 'Товар', prise: 150, description: 'Known for her sculptural takes on traditional tailoring.', image: 'product_6.webp', discount: 0 },
+            { title: 'Товар', prise: 150, description: 'Known for her sculptural takes on traditional tailoring.', image: 'product_7.webp', discount: 0 },
+            { title: 'Товар', prise: 150, description: 'Known for her sculptural takes on traditional tailoring.', image: 'product_8.webp', discount: 0 },
+            { title: 'Товар', prise: 150, description: 'Known for her sculptural takes on traditional tailoring.', image: 'product_12.webp', discount: 0 },
+        ];
+
+    }
+
+
+
+}
---
+++ js/utils/idGenerator.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let lastID = 1;
+
+export default () => lastID++; // генератор уникального id
